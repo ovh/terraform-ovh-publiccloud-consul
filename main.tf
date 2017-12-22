226f990d529f8e066cfe31bd0fc7bf4e9b664766
Yann DEGAT <yann.degat@gmail.com>
2017-12-22 12:16:21 +0100

    Fix breaking ref typo in main.tf

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "openstack_networking_port_v2" "public_port_consul" {
   count = "${var.associate_public_ipv4 ? var.count : 0}"
 
   name               = "${var.name}_consul_public_port_${count.index}"
-  network_id         = "${data.openstack_networking_subnet_v2.ext_net.network_id}"
+  network_id         = "${data.openstack_networking_network_v2.ext_net.network_id}"
   admin_state_up     = "true"
   security_group_ids = [
     "${compact(concat(list(openstack_networking_secgroup_v2.public_servers_sg.id),var.public_security_group_ids))}"

21a4243f36c1716c4e2b1ebe3f38f3537673aee0
yanndegat <yann.degat@gmail.com>
2018-02-08 09:32:38 +0100

    Update watch-cacerts.sh remove comment
    Update watch-cacerts.sh
    
     remove obsolete comment

--- modules/install-consul/watch-cacerts.sh
+++ modules/install-consul/watch-cacerts.sh
@@ -7,7 +7,6 @@ watch(){
 }
 
 handler(){
-    # if yum -> fedora family, else coreos container linux
     for i in $(jq -r '.[]|[.Key,.Value]|join(",")'); do
         echo "$i" | cut -d, -f2 | base64 -d | tee "$CACERTS_INSTALL_DIR/$(echo "$i" | cut -d, -f1 | sed 's/\//_/g' ).pem"
     done
